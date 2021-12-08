3096b1dd5d2cad040ee44e0274772118ce00227d
mattpoole84 <84345648+mattpoole84@users.noreply.github.com>
2021-12-08 12:09:56 +0200

    update crawler

---
+++ crawler.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+
+from abodeapi.api import upload_rental, add_user
+from abodeapi.utils import read_json_file, get_url
+import visitnsw.visitnsw
+import atdw.atdw
+
+#visitnsw.visitnsw.process()
+
+#queensland.queensland.process()
+
+atdw.atdw.process()
+
+
+
